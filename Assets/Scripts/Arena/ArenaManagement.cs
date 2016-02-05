1acda06487054c08d154374808ad3ee3d091dd11
Chanisco Tromp <chanisco@live.nl>
2016-02-05 15:04:16 +0100

    Character Movement Complete
    CharacterMovement
    Character Jump

---
+++ Assets/Scripts/Arena/ArenaManagement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Arena
+{
+    public class ArenaManagement : MonoBehaviour
+    {
+        [SerializeField]
+        public int AmountOfPlayers;
+
+        [SerializeField]
+        public Vector2 Player1Pos, Player2Pos;
+        
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            
+        }
+
+    }
+
+    public class PlayerData
+    {
+        int id;
+        int playerNumber;
+
+        public PlayerData()
+        {
+           // this.id
+        }
+
+    }
+}
---
+++ Assets/Scripts/Base/PlayerBase.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using Arena;
+
+public class PlayerBase : MonoBehaviour
+{
+    enum PositionAgainstPlayer
+    {
+        LeftOpponent,
+        RightOpponent
+    }
+
+    [SerializeField]
+    private KeyCode Jump, Left, Right, Down;
+    private float speed = 0.1f;
+    [SerializeField]
+    private bool OnPlatform = false;
+
+    private Rigidbody ownRigidbody;
+
+    void Awake()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public virtual void Init()
+    {
+        ownRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void SendInformationToArena()
+    {
+
+    }
+
+    //Commands
+    public virtual void BasicMovement()
+    {
+        if (Input.GetKey(Left))
+        {
+            transform.Translate(-1 * speed,0,0);
+        }
+        else if (Input.GetKey(Right))
+        {
+            transform.Translate(1 * speed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(Jump))
+        {
+            JumpCommand();
+        }
+    }
+
+    private void On2DCollision2DEnter(Collision col)
+    {
+        if(col.transform.tag == "Platform")
+        {
+            OnPlatform = true;
+        }
+    }
+
+    private void OnCollision2DExit(Collision col)
+    {
+        if (col.transform.tag == "Platform")
+        {
+            OnPlatform = false;
+        }
+    }
+
+    private void JumpCommand()
+    {
+        if (OnPlatform)
+        {
+            ownRigidbody.velocity = new Vector3(0, 7, 0);
+        }
+    }
+
+
+}
