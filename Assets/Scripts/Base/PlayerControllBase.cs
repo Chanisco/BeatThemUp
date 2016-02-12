ed15feb0759a7f540215bac8db49c41a47970f09
Chanisco Tromp <chanisco@live.nl>
2016-02-12 12:16:40 +0100

    PlayerCommands

--- Assets/Scripts/Base/PlayerControllBase.cs
+++ Assets/Scripts/Base/PlayerControllBase.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Controlls;
 
 namespace Controlls
 {
     public class PlayerControllBase : MonoBehaviour
     {
-        public void Update()
+        playerCommands Player1Settings()
         {
-
+            return playerCommands();
         }
 
-        private void BasicMovement()
+        playerCommands Player2Settings()
         {
-
+            return playerCommands();
         }
     }
 
+    public class playerCommands
+    {
+        public KeyCode left;
+        public KeyCode right;
+        public KeyCode up;
+
+        public KeyCode attack;
+
+        public playerCommands(KeyCode Left, KeyCode Right, KeyCode Up, KeyCode Attack)
+        {
+            this.left = Left;
+            this.right = Right;
+            this.up = Up;
+
+            this.attack = Attack;
+        }
+
+    }
 }
