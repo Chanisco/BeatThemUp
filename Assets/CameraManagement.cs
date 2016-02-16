5976d6c022f643f3571188114475a8ae77e50f3f
Chanisco Tromp <chanisco@live.nl>
2016-02-16 13:19:59 +0100

    New Arena added
    Shaders Inserted
    Prefab added

---
+++ Assets/CameraManagement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManagement : MonoBehaviour {
+    [SerializeField]
+    public Camera currentCamera
+
+    void Awake()
+    {
+        if(currentCamera == null)
+        {
+            currentCamera = Camera.main;
+        }
+    }
+
+}
